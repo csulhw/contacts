03e6703127d3d86c16a5bcf7b912c66771515cc8
lhw <csuhw@sina.com>
2018-02-05 11:06:08 +0800

    CSS 和 sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+/*
+ create table contacts (
+  id INT(20) not null AUTO_INCREMENT ,
+  firstName VARCHAR(30) not null,
+  lastName VARCHAR(50) not null,
+  phoneNumber varchar(13),
+  emailAddress VARCHAR (30),
+   PRIMARY KEY (id)
+);*/
